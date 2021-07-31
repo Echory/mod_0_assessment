8408f10061a84865f83290711a48fa28a1a19449
Emma Chory <emmachory@gmail.com>
2021-07-30 18:54:19 -0600

    Create oject for house

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+  location: "Boulder",
+  color: "green",
+  bedrooms: 4,
+  yearBuildt: 2004,
+  occupied: true,
+  haunted: false,
+  occupants: ['Margo', 'Adrian', 'Lana'],
+  downstairsRooms: ['Living room', 'Kitchen', 'Bathroom', 'Library']
+}
