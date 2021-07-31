fabe8bb33acef8a0433e5bc554eb936d38eea65b
Emma Chory <emmachory@gmail.com>
2021-07-30 18:46:16 -0600

    Create object for laptop

---
+++ models/modelA.js
@@ -0,0 +1,9 @@
+var laptop = {
+  brand: "Apple",
+  color: "Silver",
+  priceInDollars: 1200,
+  releaseYear: 2020,
+  mChip: true,
+  cdDrive: false,
+  applications: ['Photobooth', 'Books', 'GarageBand']
+}
