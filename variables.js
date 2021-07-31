68a88fed748be51926ba6bdf851657bcc47c95f6
Emma Chory <emmachory@gmail.com>
2021-07-30 18:38:02 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,13 @@
+var animal = "Monkey";
+var isCuddly = true;
+animal = "Elephant";
+
+var num8 = 8;
+var num3 = 3;
+var quotient = num3 / num8;
+
+var favoriteFilms = ['Tangled', 'The Hobbit', 'The Green Mile'];
+favoriteFilms.shift();
+console.log(favoriteFilms);
+
+console.log(animal.length);
